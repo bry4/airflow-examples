b076741289a9be922d6c68eeb498af3d8febafda
bryanvargasc <xt7029@rimac.com.pe>
2023-05-19 14:22:35 -0500

    add mysql database

--- dags/WorkFlow.py
+++ dags/WorkFlow.py
@@ -1,18 +1,21 @@
 import requests
 import json
+from pandas import json_normalize
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.utils.dates import days_ago
 
 
-def fetch_and_process_data():
+def get_api_data(user_id):
     response = requests.get("https://jsonplaceholder.typicode.com/users")
     data = response.json()
-    print(f"Fetched {len(data)} users.")
     for user in data:
-        print(f"User ID: {user['id']}, Name: {user['name']}")
+        if user["id"] == user_id:
+            print(user)
 
+#def transform_data():
 
 default_args = {
     'owner': 'airflow',
@@ -25,15 +28,29 @@ default_args = {
 }
 
 dag = DAG(
-    'fetch_data_dag',
+    'example_dag',
     default_args=default_args,
-    description='A DAG that fetches data from a public API',
+    description='A DAG that get data from a public API and send to Database',
     schedule_interval=None,
     start_date=days_ago(2),
 )
 
-fetch_data_task = PythonOperator(
-    task_id='fetch_data',
-    python_callable=fetch_and_process_data,
+start_task = EmptyOperator(
+    task_id='start',
     dag=dag,
 )
+
+get_data_task = PythonOperator(
+    task_id='get_data',
+    python_callable=get_api_data,
+    op_args=[4],
+    dag=dag,
+)
+
+end_task = EmptyOperator(
+    task_id='end',
+    trigger_rule='one_success',
+    dag=dag,
+)
+
+start_task >> get_data_task >> end_task
