d49de2e9e3a932e9c6249242481a35221497a28b
bryanvargasc <xt7029@rimac.com.pe>
2023-05-19 19:54:03 -0500

    final test

--- dags/WorkFlow.py
+++ dags/WorkFlow.py
@@ -1,21 +1,38 @@
 import requests
 import json
-from pandas import json_normalize
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.mysql_operator import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.utils.dates import days_ago
 
 
-def get_api_data(user_id):
+def get_api_data(**kwargs):
     response = requests.get("https://jsonplaceholder.typicode.com/users")
     data = response.json()
     for user in data:
-        if user["id"] == user_id:
-            print(user)
+        if user["id"] == 4:
+            data_dict = transform(user)
+            kwargs['ti'].xcom_push(key='data_from_api', value=data_dict)
+            #df = pd.DataFrame(data_dict)
+            #df.to_csv('/tmp/user.csv', sep='\t', index=False, header=False)
 
-#def transform_data():
+def transform(user):
+    keys = ["id","name", "username","email"]
+    new_dict = {key: user[key] for key in keys if key in user}
+    return new_dict
+
+def save_to_mysql(**kwargs):
+    ti = kwargs['ti']
+    data = ti.xcom_pull(key='data_from_api')
+    mysql_hook = MySqlHook(mysql_conn_id='mysql_webinar')
+    name = data["name"]
+    username = data["username"]
+    email = data["email"]
+    mysql_hook.run(f"INSERT INTO users (name, username, email) VALUES ('{name}', '{username}', '{email}');")
+
 
 default_args = {
     'owner': 'airflow',
@@ -28,11 +45,12 @@ default_args = {
 }
 
 dag = DAG(
-    'example_dag',
+    'workflow',
     default_args=default_args,
     description='A DAG that get data from a public API and send to Database',
     schedule_interval=None,
     start_date=days_ago(2),
+    tags=['3rd_dag']
 )
 
 start_task = EmptyOperator(
@@ -40,11 +58,16 @@ start_task = EmptyOperator(
     dag=dag,
 )
 
-get_data_task = PythonOperator(
-    task_id='get_data',
+get_data = PythonOperator(
+    task_id='get_data_from_api',
     python_callable=get_api_data,
-    op_args=[4],
-    dag=dag,
+    provide_context=True,
+)
+    
+save_data = PythonOperator(
+    task_id='save_data_to_mysql',
+    python_callable=save_to_mysql,
+    provide_context=True,
 )
 
 end_task = EmptyOperator(
@@ -53,4 +76,5 @@ end_task = EmptyOperator(
     dag=dag,
 )
 
-start_task >> get_data_task >> end_task
+start_task >> get_data >> save_data >> end_task
+
